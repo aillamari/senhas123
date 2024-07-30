f5d8a67d2b245c60774d720501c1acc817b30546
aillamari <127351793+aillamari@users.noreply.github.com>
2024-07-30 07:33:13 -0300

    \z\cav

---
+++ main.js
@@ -0,0 +1,9 @@
+conts numeroSenha = document.querySelector('.parametro')
+conts botoes = document.querySelectorAll('.parametro')
+
+botoes[0].onclick = diminuirTamanho
+
+function diminuirTamanho() {
+   console.log('teste')
+}
+numeroSenha.textContent = 6
