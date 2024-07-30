45a7d828bd92c9c77fa53c5b9b277254c38a5292
aillamari <127351793+aillamari@users.noreply.github.com>
2024-07-30 07:56:42 -0300

    a\dd

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
-botoes[0].onclick = diminuirTamanho
+botoes[0].onclick= diminuirTamanho
+
+let tamanhoSenha = 5 
+numeroSenha.textContent = tamanhoSenha
 
 function diminuirTamanho() {
-   console.log('teste')
-}
-numeroSenha.textContent = 6
+    if(!tamanhoSenha > 1)
+    tamanhoSenha = tamanhoSenha - 1
+        numeroSenha.textContent = tamanhoSenha 
+    }
+
+x
