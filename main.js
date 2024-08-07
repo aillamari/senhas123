8ad1e3f723009698452bdeab540ebe9bbd006999
aillamari <127351793+aillamari@users.noreply.github.com>
2024-08-07 09:12:15 -0300

    ńjp

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
 botoes[0].onclick= diminuirTamanho
+botoes[1].onclick= aumentarTamanho
+
 
 let tamanhoSenha = 5 
 numeroSenha.textContent = tamanhoSenha
 
+let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let minuscula = "abcedfghijklmnopqrstuvwxyz"
+let numero = "0123456789"
+let simbolos = "!?@%&*"
+
 function diminuirTamanho() {
-    if(!tamanhoSenha > 1)
+    if(tamanhoSenha > 1)
     tamanhoSenha = tamanhoSenha - 1
         numeroSenha.textContent = tamanhoSenha 
     }
 
-x
+
+function aumentarTamanho(){
+if(aumentarTamanho < 20)
+    tamanhoSenha= tamanhoSenha + 1
+numeroSenha.textContent = tamanhoSenha
+
+
+}
