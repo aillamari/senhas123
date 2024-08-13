b967727f2f4c4591274e570ba790eee7de6efa92
aillamari <127351793+aillamari@users.noreply.github.com>
2024-08-13 07:31:46 -0300

    ct

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
+const campoSenha = document.querySelector('#campo-senha')
 
 botoes[0].onclick= diminuirTamanho
 botoes[1].onclick= aumentarTamanho
@@ -18,25 +19,25 @@ geraSenha()
 function diminuirTamanho() {
     if(tamanhoSenha > 1)
     tamanhoSenha = tamanhoSenha - 1
-        numeroSenha.textContent = tamanhoSenha 
-    }
-
+    numeroSenha.textContent = tamanhoSenha
+    
+}
 
 function aumentarTamanho(){
-if(aumentarTamanho < 20)
+    if(aumentarTamanho < 20)
     tamanhoSenha= tamanhoSenha + 1
-numeroSenha.textContent = tamanhoSenha
-
+    numeroSenha.textContent = tamanhoSenha
 
 }
 
-    function geraSenha(){
-        let senha = " "
-        for(let i = 0; i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*maiuscula.length
-        numeroAleatorio = Math.floor(numeroAleatorio)
-        console.log(numeroAleatorio)
-        console.log(senha)
-    }
-    campoSenha.value = senha
-    }
+function geraSenha(){
+    let senha = " "
+    for(let i = 0; i < tamanhoSenha; i++){
+    let numeroAleatorio = Math.random()*maiuscula.length
+    numeroAleatorio = Math.floor(numeroAleatorio)
+    senha = senha + maiuscula[numeroAleatorio];
+
+    console.log(senha)
+}
+campoSenha.value = senha
+}
