6837987d8b2bddaf190c6bca462ee98d3c583788
aillamari <127351793+aillamari@users.noreply.github.com>
2024-08-27 07:38:52 -0300

    .oml]lm

--- main.js
+++ main.js
@@ -1,68 +1,59 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto')
-const botoes = document.querySelectorAll('.parametro-senha__botao')
-const campoSenha = document.querySelector('#campo-senha')
-const checkbox = document.querySelectorAll('.checkbox')
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 
-console.log(checkbox[0].checked)
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-
-botoes[0].onclick= diminuirTamanho
-botoes[1].onclick= aumentarTamanho
-
-
-let tamanhoSenha = 5 
-numeroSenha.textContent = tamanhoSenha
-
-let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let minuscula = "abcedfghijklmnopqrstuvwxyz"
-let numero = "0123456789"
-let simbolos = "!?@%&*"
-
-geraSenha()
-
-function diminuirTamanho() {
-    if(tamanhoSenha > 1){
-    tamanhoSenha = tamanhoSenha - 1
-    numeroSenha.textContent = tamanhoSenha
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
     }
-    geraSenha()
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
 
-function aumentarTamanho(){
-    if(Tamanhosenha < 20){
-    tamanhoSenha = tamanhoSenha + 1
-    numeroSenha.textContent = tamanhoSenha
-    }
-    console.log(tamanhoSenha)
-    geraSenha()
+geraSenha();
 
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
     }
-
-function geraSenha(){
-    let senha = " "
-    let alfabeto = ""
-    if (checkbox[0].checked){
-        alfabeto = alfabeto+minuscula
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
     }
-     
-    if (checkbox[1].checked){
-        alfabeto = alfabeto+minuscula
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
     }
-
-    if (checkbox[2].checked){
-        alfabeto = alfabeto+maiuscula
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
     }
-
-    if (checkbox[3].checked){
-        alfabeto = alfabeto+simbolos
-    }
-
-    for(let i =0;i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*alfabeto.length
-        numeroAleatorio = Math.floor(numeroAleatorio)
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + alfabeto[numeroAleatorio];
-        console.log(senha)
     }
-    campoSenha.value = senha
-    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+
+}
